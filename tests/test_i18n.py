8e60e4e8ba2eee94f52e4661de0d7bb4b7aeb72b
ricardomarzochi07 <ricardomarzochi@gmail.com>
2025-09-24 21:28:09 +0200

    Add_Version_Initial_0.3.0

--- tests/test_i18n.py
+++ tests/test_i18n.py
@@ -1,61 +1,23 @@
 import os
-import pytest
-from contextvars import ContextVar
 
-from buddybet_i18n.i18n_manager import I18nManager
+from buddybet_i18n.i18n_service import I18nService  # <-- Aquí importas la clase principal
 
 
-# Simula un ContextVar de idioma (como harías en el microservicio)
-test_language: ContextVar[str] = ContextVar("test_language", default="en")
+def test_token_expired_translations():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    root_dir = os.path.abspath(os.path.join(current_dir, ".."))
+    i18n = I18nService(root_dir=root_dir)
 
-def get_test_language():
-    return test_language.get()
+    i18n.set_language("en")
+    assert i18n.gettext("token_expired") == "Token has expired"
 
+    i18n.set_language("es")
+    assert i18n.gettext("token_expired") == "El token ha expirado"
 
-# Ruta del archivo de mensajes de prueba
-MESSAGES_FILE_PATH = os.path.join(os.path.dirname(__file__), "messages.json")
+    i18n.set_language("fr")
+    assert i18n.gettext("token_expired") == "Le jeton a expiré"
 
-@pytest.fixture
-def i18n():
-    return I18nManager.from_json_file(
-        path=MESSAGES_FILE_PATH,
-        default_lang="en",
-        language_getter=get_test_language
-    )
-
-
-def test_get_message_with_contextvar_es(i18n):
-    test_language.set("es")
-    msg = i18n.get_message("token_expired")
-    assert msg == "El token ha expirado"
-
-
-def test_get_message_with_contextvar_en(i18n):
-    test_language.set("en")
-    msg = i18n.get_message("token_expired")
-    assert msg == "Token has expired"
-
-
-def test_get_message_with_unsupported_lang_fallback(i18n):
-    test_language.set("de")  # alemán no está soportado
-    msg = i18n.get_message("token_expired")
-    assert msg == "Token has expired"  # fallback al default_lang
-
-
-def test_get_message_with_manual_lang_override(i18n):
-    test_language.set("es")  # Esto debería ignorarse
-    msg = i18n.get_message("token_expired", lang="fr")
-    assert msg == "Le jeton a expiré"
-
-
-def test_missing_key_returns_placeholder(i18n):
-    test_language.set("es")
-    msg = i18n.get_message("non_existent_key")
-    assert msg == "[non_existent_key]"
-
-
-def test_get_message_without_contextvar_and_without_lang(i18n):
-    # Borra el valor en el contextvar (no se ha seteado explícitamente)
-    test_language = ContextVar("test_language", default="en")
-    msg = i18n.get_message("user_not_found")
-    assert msg == "User not found"
+    """
+    i18n.set_language("de")  # idioma no existente, fallback a inglés
+    assert i18n.gettext("token_expired") == "Token has expired"
+    """
